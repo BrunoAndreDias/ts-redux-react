d860f757d1e3c27de7ebe95eb60c2930bf34c0fa
Bruno Dias <baad@sapo.pt>
2021-03-01 20:14:52 +0000

    Basic setup with typescript and react-redux

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,38 @@
+import { ActionType } from '../action-types';
+import { Action } from '../actions';
+
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
